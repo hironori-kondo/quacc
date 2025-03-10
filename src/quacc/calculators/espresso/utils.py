1719ddf612a3de5d5d9bbde5b71d1a0e083fd590
nerdwarrior <hironorikondo92@gmail.com>
2025-03-10 19:38:44 -0400

    Add wildcards to pwscf.hess and pwscf.xml paths in quacc.calculators.espresso.utils.prepare_copy_files()

--- src/quacc/calculators/espresso/utils.py
+++ src/quacc/calculators/espresso/utils.py
@@ -268,7 +268,7 @@ def prepare_copy_files(parameters: dict[str, Any], binary: str = "pw") -> list[P
     elif binary in {"ph", "phcg"}:
         to_copy.extend(pw_base)
         to_copy.append(Path("pwscf.save", "wfc*.*"))
-        to_copy.append(Path("pwscf.hess"))
+        to_copy.append(Path("pwscf.hess*"))
 
         inputph = input_data.get("inputph", {})
         ldisp = inputph.get("ldisp", False)
@@ -332,7 +332,7 @@ def prepare_copy_files(parameters: dict[str, Any], binary: str = "pw") -> list[P
         to_copy.extend([Path("ahc_dir"), Path("matdyn.modes*")])
 
     elif binary == "d3hess":
-        to_copy.append(Path("pwscf.xml"))
+        to_copy.append(Path("pwscf.xml*"))
 
     return to_copy
 
