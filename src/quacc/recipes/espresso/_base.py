7e0d8a68a32a94103332300fc6aab3811be2b8cb
nerdwarrior <hironorikondo92@gmail.com>
2025-03-16 11:03:33 -0400

    Add `rename_files` functionality to `quacc.recipes.espresso._base.run_and_summarize()` and `quacc.recipes.espresso._base.prepare_copy()`

--- src/quacc/recipes/espresso/_base.py
+++ src/quacc/recipes/espresso/_base.py
@@ -82,7 +82,7 @@ def run_and_summarize(
         calc_swaps=calc_swaps,
     )
 
-    updated_copy_files = prepare_copy(
+    updated_copy_files, rename_files = prepare_copy(
         copy_files=copy_files,
         calc_params=calc.user_calc_params,
         binary=calc.template.binary,
@@ -90,7 +90,7 @@ def run_and_summarize(
 
     geom_file = template.outputname if template and template.binary == "pw" else None
 
-    final_atoms = Runner(atoms, calc, copy_files=updated_copy_files).run_calc(
+    final_atoms = Runner(atoms, calc, copy_files=updated_copy_files, rename_files=rename_files).run_calc(
         geom_file=geom_file
     )
 
@@ -262,7 +262,7 @@ def prepare_copy(
         copy_files = [copy_files]
 
     if isinstance(copy_files, list):
-        exact_files_to_copy = prepare_copy_files(calc_params, binary=binary)
-        return {source: exact_files_to_copy for source in copy_files}
+        exact_files_to_copy, files_to_rename = prepare_copy_files(calc_params, binary=binary)
+        return {source: exact_files_to_copy for source in copy_files}, files_to_rename
 
     return copy_files
